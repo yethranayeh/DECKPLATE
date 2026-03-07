fbde0dbe96a5f11ce658cdefe73c7cb3422e6988
Alper Halil <aktasalper99@gmail.com>
2026-03-07 19:00:23 +0100

    create documentation/dev app

---
+++ apps/docs/astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+	integrations: [react(), mdx()],
+	vite: {
+		plugins: [tailwindcss()]
+	}
+});
